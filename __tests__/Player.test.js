2e9960ed919530ecc03e3380e921621383b039e9
d-taylor6403 <dannielletaylor@outlook.com>
2021-02-17 17:57:48 -0600

    player constructor complete

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -3,7 +3,7 @@ const Potion = require('../lib/Potion');
 
 jest.mock('../lib/Potion.js');
 
-console.log(new Potion());
+//console.log(new Potion());
 
 test('creates a player object', () => {
     const player = new Player('Dave');
@@ -62,4 +62,31 @@ test('subtracts from players health', () => {
     player.reduceHealth(99999);
 
     expect(player.health).toBe(0);
+});
+
+test('gets players attack value', () => {
+    const player = new Player('Dave');
+    player.strength = 10;
+
+    expect(player.getAttackValue()).toBeGreaterThanOrEqual(5);
+    expect(player.getAttackValue()).toBeLessThanOrEqual(15)
+});
+
+test('adds a potion to the inventory', () => {
+    const player = new Player('Dave');
+    const oldCount = player.inventory.length;
+
+    player.addPotion(new Potion());
+
+    expect(player.inventory.length).toBeGreaterThan(oldCount);
+});
+
+test('uses a potion from inventory', () => {
+    const player = new Player('Dave');
+    player.inventory = [new Potion(), new Potion(), new Potion()];
+    const oldCount = player.inventory.length;
+
+    player.usePotion(1);
+
+    expect(player.inventory.length).toBeLessThan(oldCount);
 });
